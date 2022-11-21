7803ce592cf0e495d205a21a95af72dc0a62e2f6
lassev05 <vestergaardlasse2@gmail.com>
2022-11-21 09:35:42 +0100

    Check command!

---
+++ src/commands/check.js
@@ -0,0 +1,64 @@
+require("dotenv").config();
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const LinkerClient = require("linker.js").Client;
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("check")
+    .setDescription("🔑 Check a link, to see if its valid...")
+    .addStringOption((option) =>
+      option
+        .setName("id")
+        .setDescription("Enter the linkId to check...")
+        .setRequired(true)
+    ),
+
+  async execute(interaction) {
+    const id = interaction.options.getString("id");
+
+    const client = new LinkerClient({
+      token: process.env.API_KEY,
+    });
+
+    if (!id) {
+      interaction.reply("**ID IS REQUIRED**");
+    } else {
+      const checkLink = client.getLink(id);
+
+      checkLink.then((link) => {
+        const linkEmbed = new EmbedBuilder()
+          .setColor(process.env.MAIN_COLOR)
+          .setTitle("Checked!")
+          .setDescription(`\n✅ Your link was checked successfully!`)
+          .addFields(
+            {
+              name: "Original link",
+              value: link.original,
+              inline: true,
+            },
+            {
+              name: "Id",
+              value: link.short,
+              inline: true,
+            }
+          )
+          .setTimestamp()
+          .setFooter({
+            text: `Requested by ${interaction.user.username}#${interaction.user.discriminator}`,
+            iconURL: `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}`,
+          })
+          .catch((e) => {
+            const errorEmbed = new EmbedBuilder()
+              .setColor("Red")
+              .setDescription(
+                `❌ The provided id is not valid... Please try agian with a valid id`
+              );
+
+            interaction.reply({ embeds: [errorEmbed] });
+          });
+
+        interaction.reply({ embeds: [linkEmbed] });
+      });
+    }
+  },
+};
--- src/commands/create.js
+++ src/commands/create.js
@@ -8,43 +8,58 @@ module.exports = {
     .setDescription("🔗 Create your link short!")
     .addStringOption((option) =>
       option
-        .setName("url")
-        .setDescription("Enter your url to short here...")
+        .setName("link")
+        .setDescription("Enter your link to short...")
         .setRequired(true)
     ),
 
   async execute(interaction) {
-    const url = interaction.options.getString("url");
+    const url = interaction.options.getString("link");
 
     const client = new LinkerClient({
       token: process.env.API_KEY,
     });
 
     if (!url) {
-      interaction.reply("Please enter a URL!");
+      interaction.reply("**LINK IS REQUIRED**");
     } else {
       const newLink = client.create(url);
 
-      newLink.then((link) => {
-        const linkEmbed = new EmbedBuilder()
-          .setColor(process.env.MAIN_COLOR)
-          .setTitle("Created!")
-          .setDescription(`✅ Your link **${url}** was created successfully!`)
-          .addFields(
-            {
-              name: "Original link",
-              value: link.original,
-              inline: true,
-            },
-            {
-              name: "Shorted link",
-              value: link.html_url,
-              inline: true,
-            }
-          );
+      newLink
+        .then((link) => {
+          const linkEmbed = new EmbedBuilder()
+            .setColor(process.env.MAIN_COLOR)
+            .setTitle("Created!")
+            .setDescription(`\n✅ Your link was shorted successfully!`)
+            .addFields(
+              {
+                name: "Original link",
+                value: link.original,
+                inline: true,
+              },
+              {
+                name: "Shorted link",
+                value: link.html_url,
+                inline: true,
+              }
+            )
+            .setTimestamp()
+            .setFooter({
+              text: `Requested by ${interaction.user.username}#${interaction.user.discriminator}`,
+              iconURL: `https://cdn.discordapp.com/avatars/${interaction.user.id}/${interaction.user.avatar}`,
+            });
 
-        interaction.reply({ embeds: [linkEmbed] });
-      });
+          interaction.reply({ embeds: [linkEmbed] });
+        })
+        .catch((e) => {
+          const errorEmbed = new EmbedBuilder()
+            .setColor("Red")
+            .setDescription(
+              `❌ Your link is invalid... Please try agian with a valid link`
+            );
+
+          interaction.reply({ embeds: [errorEmbed] });
+        });
     }
   },
 };
